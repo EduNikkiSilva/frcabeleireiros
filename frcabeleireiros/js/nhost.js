e123ffdd93756f1e790a48a9135a5917a6978273
Eduardo Silva <126163376+EduNikkiSilva@users.noreply.github.com>
2026-07-26 00:18:20 +0100

    Delete frcabeleireiros directory

--- frcabeleireiros/js/nhost.js
+++
@@ -1,6 +0,0 @@
-import { NhostClient } from "https://cdn.jsdelivr.net/npm/@nhost/nhost-js@2/+esm";
-
-export const nhost = new NhostClient({
-  subdomain: "vstlzonsmlxhhbdpoisn",
-  region: "eu-central-1"
-});
